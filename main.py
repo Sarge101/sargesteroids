b5900f91e925f4676ec874d0f85db2da81896264
Sarge101 <rabobi@hotmail.co.uk>
2025-10-02 15:53:39 +0200

    added fps60 restrictor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import pygame
-pygame.init()
 
 from constants import *
 
@@ -9,17 +8,20 @@ def main():
 Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}""")
     
+    pygame.init()
+
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
      
     running = True
     while running:
-        # Clear the screen with black
         screen.fill((0, 0, 0))
-
-        # Update the display
         pygame.display.flip()
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+        print(dt)
 
-        # Handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
