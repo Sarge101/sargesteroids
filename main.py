6a9963dba5aabd30038efea6af455efaf03fd5f1
Sarge101 <rabobi@hotmail.co.uk>
2025-10-03 11:54:42 +0200

    added player.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import pygame
-
 from constants import *
-
+from player import Player
 
 def main():
     print(f"""Starting Asteroids!
@@ -13,10 +12,12 @@ Screen height: {SCREEN_HEIGHT}""")
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
      
     running = True
     while running:
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
