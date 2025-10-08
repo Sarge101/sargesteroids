3c4a0c21c5526cd4caba842118894ccdd432d493
Sarge101 <rabobi@hotmail.co.uk>
2025-10-08 12:04:18 +0200

    added collision

--- circleshape.py
+++ circleshape.py
@@ -1,6 +1,5 @@
 import pygame
 
-# Base class for game objects
 class CircleShape(pygame.sprite.Sprite):
     def __init__(self, x, y, radius):
         # we will be using this later
@@ -20,3 +19,9 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt):
         # sub-classes must override
         pass
+
+    def collides_with(self, other):
+        """Return True if this circle overlaps with another CircleShape."""
+        distance_sq = self.position.distance_squared_to(other.position)
+        radius_sum_sq = (self.radius + other.radius) ** 2
+        return distance_sq <= radius_sum_sq
